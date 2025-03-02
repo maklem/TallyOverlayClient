69a2e421efcd1e2229ee08de5976fe436d4d3df6
Marco <marco-git@heavens-inc.de>
2025-03-02 08:55:23 +0100

    tests: skips tests dependent on UI

--- tests/test_create_ui_classes.py
+++ tests/test_create_ui_classes.py
@@ -1,19 +1,41 @@
+import sys
 import tkinter as tk
+
+from typing import Generator
+
 import pytest
 
 from tallyoverlayclient.models import AppDataModel
 from tallyoverlayclient.ui import ConfigFrame, ConfigFrameViewModel, OverlayWindow, OverlayWindowViewModel
 
 
-def test_createConfigFrame() -> None:
-    root = tk.Tcl()
+def tk_is_available() -> bool:
+    try:
+        tk.Tk().quit()
+        return True
+    except tk.TclError:
+        return False
+
+
+@pytest.fixture
+def tk_root() -> Generator[tk.Tk, None, None]:
+    root = tk.Tk()
+    yield root
+    root.quit()
+
+
+@pytest.mark.skipif(not tk_is_available(), reason="Tkinter is not available")
+def test_createConfigFrame(tk_root: tk.Tk) -> None:
     view_model = ConfigFrameViewModel(AppDataModel())
-    frame = ConfigFrame(root, view_model)
+    frame = ConfigFrame(tk_root, view_model)
     assert frame is not None
 
 
-def test_createOverlayWindow() -> None:
-    root = tk.Tcl()
+@pytest.mark.skipif(not tk_is_available(), reason="Tkinter is not available")
+@pytest.mark.skipif(
+    sys.platform != "win32", reason="Overlay window uses Windows specific APIs"
+)
+def test_createOverlayWindow(tk_root: tk.Tk) -> None:
     view_model = OverlayWindowViewModel(AppDataModel())
-    overlay = OverlayWindow(root, view_model)
+    overlay = OverlayWindow(tk_root, view_model)
     assert overlay is not None
--- tests/test_property.py
+++ tests/test_property.py
@@ -1,22 +1,28 @@
 import tkinter as tk
+from typing import Generator
 import pytest
 from tallyoverlayclient.models import Property
 
 
-def test_whenPropertyChanges_boundVariableUpdates() -> None:
+@pytest.fixture
+def tcl() -> Generator[tk.Tk, None, None]:
     tcl = tk.Tcl()
+    yield tcl
+    tcl.quit()
+
+
+def test_whenPropertyChanges_boundVariableUpdates(tcl: tk.Tk) -> None:
     property = Property[str]("test")
-    bound_variable = property.bind_to_tkinter(tk.StringVar())
+    bound_variable = property.bind_to_tkinter(tk.StringVar(master=tcl))
     assert bound_variable.get() == "test"
 
     property.set("new value")
     assert bound_variable.get() == "new value"
 
 
-def test_whenVariableChanges_propertyIsUpdated() -> None:
-    tcl = tk.Tcl()
+def test_whenVariableChanges_propertyIsUpdated(tcl: tk.Tk) -> None:
     property = Property[str]("test")
-    bound_variable = property.bind_to_tkinter(tk.StringVar())
+    bound_variable = property.bind_to_tkinter(tk.StringVar(master=tcl))
 
     bound_variable.set("new value")
     assert property.get() == "new value"
