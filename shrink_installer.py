803bef168d7a82e999933ae6f96cf63c69b1fffa
Marco <marco-git@heavens-inc.de>
2025-03-02 14:13:28 +0100

    installer: removes unused files from _tcl_data

---
+++ shrink_installer.py
@@ -0,0 +1,8 @@
+import shutil
+
+shutil.rmtree("dist/main/_internal/_tcl_data/encoding")
+shutil.rmtree("dist/main/_internal/_tcl_data/tzdata")
+shutil.rmtree("dist/main/_internal/_tcl_data/msgs")
+shutil.rmtree("dist/main/_internal/_tcl_data/http1.0")
+shutil.rmtree("dist/main/_internal/_tk_data/msgs")
+shutil.rmtree("dist/main/_internal/_tk_data/images")
